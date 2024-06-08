bf04d02a03c5c01c51ccb9bb86ba37392d192d2d
bekalu73 <bekalusisay2010@gmail.com>
2024-06-08 14:35:05 +0300

    day 1 python challenge

---
+++ day_01/main.py
@@ -0,0 +1,21 @@
+# print('"hello world"\n"hello world"\n"hello world"');
+# name=input("enter your name,please ")
+# input("")
+# print(len(name))
+
+# Get inputs from the user
+a = input("Enter a: ")
+b = input("Enter b: ")
+
+# Print the original values
+print("Original a:", a)
+print("Original b:", b)
+
+# Swap the values using a temporary variable
+temp = a
+a = b
+b = temp
+
+# Print the swapped values
+print("Swapped a:", a)
+print("Swapped b:", b)
