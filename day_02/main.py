b91ca9ca371737b00b7f81e362168e076436a9fa
bekalu73 <bekalusisay2010@gmail.com>
2024-06-09 20:02:30 +0300

    day 2 challenge

---
+++ day_02/main.py
@@ -0,0 +1,37 @@
+#Data Types
+# charNum= str(len(input("enter your name\n")))
+# print(type(charNum))
+# a=int(charNum)
+# print(type(a))
+
+#challenge
+# value= input("Enter your Two Number: \n")
+# num1= value[0];
+# num2= value[1];
+# sum=int(num1)+int(num2);
+
+# my code
+# print(sum)
+
+# challenge 2
+# BMI Calculator
+# height= float(input())
+# weight= float(input())
+
+
+# maxNum= max(height, weight);
+# minNum= min(height, weight);
+
+# BMI= (maxNum)//minNum**2;
+# print("Your BMI is:", BMI)
+
+# print(int(8/3))
+
+# challenge-2
+# Life in Weeks
+age=input("How Old are you?\n")
+weeks=(90-int(age))*52;
+
+print(f"you have {weeks} Weeks Left! Enjoy it!")
+
+
