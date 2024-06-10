19a4642e30372d69baf7b2cc296f77f8fa66c790
bekalu73 <bekalusisay2010@gmail.com>
2024-06-10 20:58:51 +0300

    day-3 challenge

---
+++ day_03/main.py
@@ -0,0 +1,77 @@
+# Comditionals
+# number= int(input("Enter a Number Please!\n"))
+# if(number%2==0):
+#     print(f"{number} is Even Number");
+# else:
+#     print(f"{number} is Odd Number")
+
+# challenge-1
+# BMI Calculator
+# print("BMI Calculator")
+# height= float(input("Enter Your Height\n"))
+# weight= float(input("Enter Your Weight\n"))
+
+
+# maxNum= max(height, weight);
+# minNum= min(height, weight);
+
+# BMI= (maxNum)//minNum**2;
+# if(BMI<16):
+#     print(f"Your BMI is { BMI} Your are UnderWeight")
+# elif BMI<25:
+#     print(f"Your BMI is { BMI} Your are Normal Weight")
+# elif BMI<30:
+#     print(f"Your BMI is { BMI} Your are Obese")
+# else:
+#      print(f"Your BMI is { BMI} Your are Clinically Obese")
+
+
+# challenge-2
+# Leap Year
+# print("Leap Calculator")
+# year= int(input("Enter Any Year please\n"))
+
+# if(year%4==0):
+#     if(year%100==0):
+#         if(year%400==0):
+#            print(f"{year} is  Leap Year")  
+#         else:
+#             print(f"{year} is Not Leap Year")   
+
+#     else:
+#         print(f"{year} is Leap Year")    
+# else :
+#     print(f"{year} is Not Leap Year")   
+
+# challenge-3
+#Python Pizza 
+# print("Thank You for choosing Python Pizza Deliveries")
+# size=input("What Size Do you want? S, M or L ?\n")
+# add_papperorni=input("Do you want papperorni? Y or N\n")
+# extra_cheese=input("Do you want cheese? Y or N\n")
+
+# bill=0
+# if(size=="S"):
+#     bill+=15
+#     if(add_papperorni=="Y"):
+#         bill+=2
+#     if(extra_cheese=="Y"):
+#         bill+=1
+
+# elif (size=="M"):
+#     bill+=20
+#     if(add_papperorni=="Y"):
+#         bill+=3
+#     if(extra_cheese=="Y"):
+#         bill+=1
+# elif(size=="L"):
+#     bill+=25
+#     if(add_papperorni=="Y"):
+#         bill+=3
+#     if(extra_cheese=="Y"):
+#         bill+=1
+
+#     print(f"Thank You for choosing Python Pizza Deliveries, your final Bill is {bill}")
+
+
+# challenge-3
