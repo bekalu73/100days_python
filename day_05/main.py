c075ca0a0db339a5bb5a25a6e34a8239528f7c96
bekalu73 <bekalusisay2010@gmail.com>
2024-06-15 10:21:21 +0300

    day05_challenge

--- day_05/main.py
+++ day_05/main.py
@@ -30,15 +30,15 @@
 
 # challenge-3
 # FizzBuzz change
-for i in range(1,101):
-    if(i%3==0 and i%5==0):
-        print(f"Fizz Buzz")
-    elif(i%3==0):
-        print(f"Fizz")
-    elif(i%5==0):
-        print(f"Buzz")
-    else:
-            print(i)
+# for i in range(1,101):
+#     if(i%3==0 and i%5==0):
+#         print(f"Fizz Buzz")
+#     elif(i%3==0):
+#         print(f"Fizz")
+#     elif(i%5==0):
+#         print(f"Buzz")
+#     else:
+#             print(i)
 
     
     
